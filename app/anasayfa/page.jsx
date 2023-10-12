6df862c2a52d53d59e4b4729374d5e1e2ee913e6
serkan-bayram <serkanbay02@hotmail.com>
2023-10-13 01:07:47 +0300

    initial commit

---
+++ app/anasayfa/page.jsx
@@ -0,0 +1,205 @@
+import { FaAngleDown } from "react-icons/fa6";
+import Image from "next/image";
+
+function SmallStars() {
+  return (
+    <div class="flex items-center space-x-1 ">
+      <svg
+        class="w-4 h-4 text-yellow-300"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 22 20"
+      >
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+      </svg>
+      <svg
+        class="w-4 h-4 text-yellow-300"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 22 20"
+      >
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+      </svg>
+      <svg
+        class="w-4 h-4 text-yellow-300"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 22 20"
+      >
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+      </svg>
+      <svg
+        class="w-4 h-4 text-yellow-300"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 22 20"
+      >
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+      </svg>
+      <svg
+        class="w-4 h-4 text-gray-300 dark:text-gray-500"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 22 20"
+      >
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+      </svg>
+    </div>
+  );
+}
+
+function Person({ name, id }) {
+  return (
+    <li>
+      <div
+        className={`${
+          id % 2 == 0 ? "bg-secondary" : "bg-primary"
+        } flex justify-between items-center text-white rounded-sm p-2`}
+      >
+        {name}
+        <SmallStars />
+      </div>
+    </li>
+  );
+}
+
+function Others() {
+  return (
+    <div className="flex flex-col px-8 pt-12 ">
+      <div className="text-gray-500 pb-1">Diğer Oylamalar</div>
+      <ul className="mb-8">
+        <Person name={"Serkan"} id={1} />
+        <Person name={"Tolga"} id={2} />
+        <Person name={"Furkan"} id={3} />
+        <Person name={"Aga"} id={4} />
+      </ul>
+    </div>
+  );
+}
+
+function Send() {
+  return (
+    <div className="flex items-center gap-1 pt-6 mx-20">
+      <button className="bg-primary w-full py-2 rounded-sm text-white border border-gray-700 appearance-none focus:ring ring-secondary shadow-md">
+        Gönder
+      </button>
+    </div>
+  );
+}
+
+function Rating() {
+  return (
+    <div className="flex flex-col items-center bg-primary mx-20 rounded-sm shadow-md mt-4 p-6 border border-gray-700">
+      <h1 className="text-white text-center font-bold">Genel Puan</h1>
+      <div class="flex items-center space-x-2 pt-4 ">
+        <svg
+          class="w-6 h-6 text-yellow-300"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 22 20"
+        >
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <svg
+          class="w-6 h-6 text-yellow-300"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 22 20"
+        >
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <svg
+          class="w-6 h-6 text-yellow-300"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 22 20"
+        >
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <svg
+          class="w-6 h-6 text-yellow-300"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 22 20"
+        >
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <svg
+          class="w-6 h-6 text-gray-300 dark:text-gray-500"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 22 20"
+        >
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+function ImageInfo() {
+  const info = [
+    "Tavuk Suyu Çorba",
+    "Nohut",
+    "Şehriyeli Pirinç Pilavı",
+    "Puding",
+  ];
+
+  return (
+    <div>
+      <ul>
+        {info.map((food) => {
+          return <li className="text-white my-2 text-center">{food}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+function Food() {
+  return (
+    <div className="flex flex-col items-center pt-8 gap-6">
+      <Image
+        src="/food.jpeg"
+        width={250}
+        height={250}
+        alt="Bugünün yemeği."
+        priority={true}
+      />
+      <ImageInfo />
+    </div>
+  );
+}
+
+function DatePicker() {
+  return (
+    <div className="flex justify-center items-center gap-1 pt-8">
+      <button className="flex gap-1 items-center py-2 px-5 text-xs rounded-full text-white  appearance-none focus:ring ring-primary">
+        Bugün
+        <FaAngleDown />
+      </button>
+    </div>
+  );
+}
+
+export default function Page() {
+  return (
+    <div>
+      <DatePicker />
+      <Food />
+      <Rating />
+      <Send />
+      <Others />
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#282932",
+        secondary: "#181A1E",
+        background: "#1E1F23",
       },
     },
   },
   plugins: [],
-}
+};
