0c5749bee91a2161467985342594174aaf62dba3
serkan-bayram <serkanbay02@hotmail.com>
2023-10-21 14:11:39 +0300

    update if rating is already saved

--- app/anasayfa/components/saveRating.jsx
+++ app/anasayfa/components/saveRating.jsx
@@ -6,16 +6,31 @@ import PocketBase from "pocketbase";
 export async function saveRating(FormData) {
   const { rating } = Object.fromEntries(FormData);
   const { session } = await getSession();
+  const userId = session.user.record.id;
+
   // Create pocketbase instance
   const pb = new PocketBase("http://127.0.0.1:8090");
 
-  // Auth as admin to create user
+  // Auth as admin
   await pb.admins.authWithPassword(
     process.env.dbUsername,
     process.env.dbPassword
   );
 
-  // update data if it's already saved
+  let isAlreadySaved = false;
+  // Will update according to this id
+  let recordId;
+  try {
+    // This throws an error if nothing is found
+    const record = await pb
+      .collection("ratings")
+      .getFirstListItem(`user.id="${userId}"`);
+    recordId = record.id;
+    isAlreadySaved = true;
+  } catch (error) {
+    isAlreadySaved = false;
+    console.log("Error:", error.response.message);
+  }
 
   // example create data
   const data = {
@@ -23,9 +38,18 @@ export async function saveRating(FormData) {
     user: session.user.record.id,
   };
 
-  try {
-    const record = await pb.collection("ratings").create(data);
-  } catch (error) {
-    console.log("Error:", error);
+  // update record if it is already saved
+  if (isAlreadySaved) {
+    try {
+      const record = await pb.collection("ratings").update(`${recordId}`, data);
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  } else {
+    try {
+      const record = await pb.collection("ratings").create(data);
+    } catch (error) {
+      console.log("Error:", error);
+    }
   }
 }
