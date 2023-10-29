668403a8c48104ced8c206918160bc125acece71
serkan-bayram <serkanbay02@hotmail.com>
2023-10-29 22:10:43 +0300

    trying production

--- app/components/Functions/authAsAdmin.jsx
+++ app/components/Functions/authAsAdmin.jsx
@@ -2,6 +2,7 @@ import PocketBase from "pocketbase";
 
 export async function authAsAdmin() {
   const pb = new PocketBase("https://yemek-oyla.pockethost.io/");
+  console.log(process.env.dbUsername, process.env.dbPassword);
 
   await pb.admins.authWithPassword(
     process.env.dbUsername,
