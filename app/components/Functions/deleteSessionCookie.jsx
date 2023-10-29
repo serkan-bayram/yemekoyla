082c729efddaa2b2958fe65ee62bc1e077185744
serkan-bayram <serkanbay02@hotmail.com>
2023-10-29 22:50:38 +0300

    fix delete cookie issue on deployment

--- app/components/Functions/deleteSessionCookie.jsx
+++ app/components/Functions/deleteSessionCookie.jsx
@@ -2,7 +2,9 @@ import { cookies } from "next/headers";
 
 export function deleteSessionCookie() {
   const cookieStore = cookies();
-  cookieStore.delete("next-auth.session-token");
+  const isLocal = cookieStore.has("next-auth.session-token");
+  if (isLocal) cookieStore.delete("next-auth.session-token");
   // production
-  cookieStore.delete("__Secure-next-auth.session-token");
+  const isProd = cookieStore.has("__Secure-next-auth.session-token");
+  if (isProd) cookieStore.delete("__Secure-next-auth.session-token");
 }
