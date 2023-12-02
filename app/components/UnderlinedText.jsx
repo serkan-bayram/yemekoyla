4769df212fc42bdb926b31e11065e99f30881bdf
serkan-bayram <serkanbay02@hotmail.com>
2023-12-02 16:56:05 +0300

    cool underline style

--- app/components/UnderlinedText.jsx
+++ app/components/UnderlinedText.jsx
@@ -1,10 +1,13 @@
 export default function UnderlinedText({ text }) {
   return (
     <button
-      className="relative
+      className={`relative
     after:transition-all after:duration-300
-    after:h-1 after:bg-accent after:absolute after:left-0 after:bottom-0
-    after:scale-y-50 underlined-link"
+    after:h-1 after:bg-accent after:absolute after:bottom-0
+    after:scale-y-75  
+    hover:after:left-0 hover:after:w-full
+    after:right-0 after:w-0
+    `}
     >
       {text}
     </button>
