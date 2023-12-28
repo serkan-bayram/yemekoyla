5878a046b38a95b8355652972d7bc7315047518e
serkan-bayram <serkanbay02@hotmail.com>
2023-12-28 20:28:54 +0300

    guest layouts

--- app/oyla/components/GuestPage.jsx
+++ app/oyla/components/GuestPage.jsx
@@ -1,9 +1,14 @@
+import Navbar from "../../components/Navbar/Navbar";
+import Food from "./Food";
+import Notifications from "./Notifications";
+import SnowfallClient from "./SnowfallClient";
+
 export default function GuestPage({ pb, menu, emojis }) {
   return (
     <div className="pt-12">
       <Notifications pb={pb} />
       <SnowfallClient />
-      <Navbar />
+      {/* <Navbar /> */}
       <div
         className="mb-12 mt-6 w-fit mx-auto p-8 rounded-sm
         border bg-secondary h-fit 
