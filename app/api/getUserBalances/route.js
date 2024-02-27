1d9d58fdb685cb60af905a23fd4de110181ebb05
Serkan <serkanbay02@hotmail.com>
2024-02-27 21:36:46 +0300

    update getUserBalances

--- app/api/getUserBalances/route.js
+++ app/api/getUserBalances/route.js
@@ -1,7 +1,5 @@
 import { isRequestSafe } from "../../components/Functions/Telegram/isRequestSafe";
 import pb from "../../components/Functions/authAsAdmin";
-import AES from "crypto-js/aes";
-import Utf8 from "crypto-js/enc-utf8";
 
 export async function POST(request) {
   const response = JSON.parse(await request.json());
@@ -10,18 +8,6 @@ export async function POST(request) {
 
   const records = await pb.collection("balance").getFullList({
     sort: "-created",
-  });
-
-  const secret = process.env.TELEGRAM_TOKEN;
-
-  // Passwords are encrypted in database, so we decrypt
-  // them before we send them to python script
-  records.forEach((record) => {
-    const bytes = AES.decrypt(record.sofra_password, secret);
-
-    const originalText = bytes.toString(Utf8);
-
-    record.sofra_password = originalText;
   });
 
   return Response.json({ users: records });
